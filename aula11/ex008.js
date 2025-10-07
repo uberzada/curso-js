047fc5d2e497f7f0e7988f9e0adabf56f7dbef4f
Bernardo Henrique Pereira <bernardohp15@gmail.com>
2025-10-06 22:46:21 -0300

    edittttttt

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 445
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+else {
+    console.log('Parabéns, você não foi multado')
+}
+console.log('Dirija sempre usando cinto de segurança!')
